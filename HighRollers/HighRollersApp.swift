91356def35c8a88737a933a2a307368b1b97fcbb
Scott Crowder <scrowder@gmail.com>
2024-03-21 10:19:59 -0500

    Initial Commit

---
+++ HighRollers/HighRollersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighRollersApp.swift
+//  HighRollers
+//
+//  Created by SCOTT CROWDER on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct HighRollersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
